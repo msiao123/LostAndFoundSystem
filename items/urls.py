7a5d8d3cc7a7b09e75f56c10f14f64901a60f70d
msiao123 <154373224+msiao123@users.noreply.github.com>
2025-05-15 13:49:03 +0800

    sixth commit

--- items/urls.py
+++ items/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('report/', views.report_lost_item, name='report_lost_item'),
     path('', views.item_list, name='item_list'),
     path('<int:item_id>/', views.item_detail, name='item_detail'),
+    path('<int:item_id>/edit/', views.edit_item, name='edit_item'),
+    path('<int:item_id>/delete/', views.delete_item, name='delete_item'),
 ]
--- items/views.py
+++ items/views.py
@@ -41,3 +41,37 @@ from django.shortcuts import get_object_or_404
 def item_detail(request, item_id):
     item = get_object_or_404(LostItem, id=item_id)
     return render(request, 'items/item_detail.html', {'item': item})
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from .forms import LostItemForm  # We'll create this next
+
+@login_required
+def edit_item(request, item_id):
+    item = get_object_or_404(LostItem, id=item_id)
+
+    if request.user != item.user:
+        return HttpResponseForbidden("You are not allowed to edit this item.")
+
+    if request.method == 'POST':
+        form = LostItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item_detail', item_id=item.id)
+    else:
+        form = LostItemForm(instance=item)
+
+    return render(request, 'items/edit_item.html', {'form': form, 'item': item})
+
+@login_required
+def delete_item(request, item_id):
+    item = get_object_or_404(LostItem, id=item_id)
+
+    if request.user != item.user:
+        return HttpResponseForbidden("You are not allowed to delete this item.")
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item_list')
+
+    return render(request, 'items/delete_item.html', {'item': item})
