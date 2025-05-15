7dfabe676fd64860329e23cc3beabd979e350243
msiao123 <154373224+msiao123@users.noreply.github.com>
2025-05-15 13:10:42 +0800

    third commit

--- items/urls.py
+++ items/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('report/', views.report_lost_item, name='report_lost_item'),
+    path('', views.item_list, name='item_list'),
 ]
--- items/views.py
+++ items/views.py
@@ -14,3 +14,9 @@ def report_lost_item(request):
     else:
         form = LostItemForm()
     return render(request, 'items/report_lost_item.html', {'form': form})
+
+from .models import LostItem
+
+def item_list(request):
+    items = LostItem.objects.order_by('-created_at')
+    return render(request, 'items/item_list.html', {'items': items})
